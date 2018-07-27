b34c008b0e818cf2b60c6faf91a3fc2180b041d6
Heshie Brody <4hbrody@gmail.com>
2018-07-27 02:03:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -14,4 +14,9 @@
 def speak_to_grandma?(talk)
   if talk == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif talk == "Hi Nana, how are you?"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif talk ="Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
+
 end
